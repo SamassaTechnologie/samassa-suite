8542b033d586aa1d95b21e99e9275e67014716d9
SamassaTechnologie <samassatechnologie10@gmail.com>
2025-08-02 13:21:16 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,7 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
